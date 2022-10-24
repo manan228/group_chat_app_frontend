646c32bd50a77b6a3f59de9af0d32a62eb5a3068
Manan <manan228@yahoo.in>
2022-10-24 17:06:06 +0530

    Added error handling and frontend of Signup

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,31 +1,67 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef, useState } from "react";
 
 function SignUp() {
-  const onSignUpFormHandler = (e) => {
+  const [error, setError] = useState(false);
+
+  const nameInputRef = useRef();
+  const emailInputRef = useRef();
+  const phoneInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const onSignUpFormHandler = async (e) => {
     e.preventDefault();
+
     console.log(`on singup form handler clicked`);
+
+    const name = nameInputRef.current.value;
+    const email = emailInputRef.current.value;
+    const phone = phoneInputRef.current.value;
+    const password = passwordInputRef.current.value;
+
+    const signUpObj = {
+      name,
+      email,
+      phone,
+      password,
+    };
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/signup",
+        signUpObj
+      );
+
+      console.log(response.data);
+
+      alert("successfully signed up")
+    } catch (err) {
+      console.log(err.response.data);
+      setError("user already exist, please Login in");
+    }
   };
   return (
     <>
       <form onSubmit={onSignUpFormHandler}>
         <div>
           <label>Name</label>
-          <input required/>
+          <input ref={nameInputRef} required />
         </div>
         <div>
           <label>Email</label>
-          <input type='email' required/>
+          <input ref={emailInputRef} type="email" required />
         </div>
         <div>
           <label>Phone No.</label>
-          <input type='number' required/>
+          <input ref={phoneInputRef} type="number" required />
         </div>
         <div>
           <label>Password</label>
-          <input type='password' required/>
+          <input ref={passwordInputRef} type="password" required />
         </div>
         <button>Sign Up</button>
       </form>
+      {error && <div>{error}</div>}
     </>
   );
 }
