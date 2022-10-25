4ef556a8c975dbd92b23da7d480657a77677fd48
Manan <manan228@yahoo.in>
2022-10-26 03:46:18 +0530

    Added online api request with useEffect

--- src/GroupChatApp.jsx
+++ src/GroupChatApp.jsx
@@ -1,23 +1,51 @@
 import axios from "axios";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const GroupChatApp = () => {
+  const [messages, setMessages] = useState([]);
+  const [online, setOnline] = useState([]);
+
   const messageInputRef = useRef();
 
   const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    const onLineUsers = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/online-users");
+
+        console.log(response.data);
+
+        setOnline(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setInterval(() => {
+      onLineUsers();
+    }, 1000);
+
+    // onLineUsers();
+  }, []);
 
   useEffect(() => {
     const getAllMessages = async () => {
       try {
         const response = await axios.get("http://localhost:3000/all-messages");
 
-        console.log(response.data.response)
+        console.log(response.data);
+        setMessages(response.data.response);
       } catch (err) {
         console.log(err);
       }
     };
 
-    getAllMessages();
+    setInterval(() => {
+      getAllMessages();
+    }, 1000);
+
+    // getAllMessages();
   }, []);
 
   const onSendClickHandler = async () => {
@@ -41,9 +69,16 @@ const GroupChatApp = () => {
   return (
     <>
       <div>GroupChatApp</div>
-      <ul>
-        <li>You Joined</li>
-      </ul>
+      {online.map(({ username }) => {
+        return <div>{username} Joined!!</div>;
+      })}
+      {messages.map((message) => {
+        return (
+          <div>
+            {message.userEmail}: {message.message}
+          </div>
+        );
+      })}
       <input ref={messageInputRef} required />
       <button onClick={onSendClickHandler}>Send</button>
     </>
